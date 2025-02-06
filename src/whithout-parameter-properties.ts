1768d433d7ef0d64f40107738eb6b456f653f78c
Eric Bermúdez Hernández <alu0101517476@ull.edu.es>
2025-02-06 18:27:56 +0000

    ejemplo clase sin las propiedades como parametros

---
+++ src/whithout-parameter-properties.ts
@@ -0,0 +1,11 @@
+class Car {
+  brand: string;
+  model: string;
+  year: number;
+
+  constructor(brand: string, model: string, year: number) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+}
